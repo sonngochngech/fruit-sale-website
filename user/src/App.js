6a78d04c04e6510eacdd47d48198a58d9d8a61a3
sonngochngech <103894614+sonngochngech@users.noreply.github.com>
2024-05-08 22:24:26 +0700

    feature: update route

--- user/src/App.js
+++ user/src/App.js
@@ -8,19 +8,65 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Resetpassword from './pages/Resetpassword';
 import Forgotpassword from './pages/Forgotpassword';
-import ProductCategory1 from './pages/productCategory1';
-
+import OurStore from './pages/OurStore';
+import SingleFruit from './pages/SingleFruit';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Order from './pages/Order';
+import Notification from './pages/Notification';
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* <Route index element={<Home />} /> */}
           <Route path="signup" element={<OpenRoutes><Signup /></OpenRoutes>} ></Route>
           <Route path="login" element={<OpenRoutes><Login /></OpenRoutes>}></Route>
           <Route path="forgot-password" element={<Forgotpassword />} />
           <Route path="reset-password" element={<Resetpassword />} />
+        <Route path="/" element={<PrivateRoutes><Layout /></PrivateRoutes>}>
+          <Route path="fruits" element={<PrivateRoutes><OurStore /></PrivateRoutes>} />
+            <Route
+              path="fruits/:id"
+              element={
+                <PrivateRoutes>
+                  <SingleFruit />
+                </PrivateRoutes>
+              }
+            />
+            <Route
+              path="cart"
+              element={
+                <PrivateRoutes>
+                  <Cart />
+                </PrivateRoutes>
+              }
+            />
+             <Route
+              path="checkout"
+              element={
+                <PrivateRoutes>
+                  <Checkout />
+                </PrivateRoutes>
+              }
+            />
+              <Route
+              path="order"
+              element={
+                <PrivateRoutes>
+                  <Order />
+                </PrivateRoutes>
+              }
+            />
+
+              <Route
+              path="notification"
+              element={
+                <PrivateRoutes>
+                  <Notification />
+                </PrivateRoutes>
+              }
+            />
+         
         </Route>
       </Routes>
     </BrowserRouter>
