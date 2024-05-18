c408e0c6e7093f93355ba9b8eeb176d666bb0656
hasonsk <sonhh2003@gmail.com>
2024-05-19 06:42:47 +0700

    add skeleton for orders

--- admin-app/src/pages/order/OrderManagement.js
+++ admin-app/src/pages/order/OrderManagement.js
@@ -1,42 +1,71 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOrders, deleteOrder } from "../../features/orders/orderSlice";
-import OrderCard from "./OrderCard"; // Import the OrderCard component
+import OrderCard from "./OrderCard"; 
+import { Skeleton, Box, Typography, Grid, Container } from '@mui/material';
 import { ToastContainer } from 'react-toastify';
 
 const OrderManagement = () => {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.orders.orders);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchOrders());
+    dispatch(fetchOrders())
+      .then(() => setLoading(false))
+      .catch((error) => {
+        console.error("Error fetching orders:", error);
+        setLoading(false);
+      });
   }, [dispatch]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  let filteredOrders;
-
-  try {
-    filteredOrders = orders.orders.filter((order) =>
-      order.email.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  } catch (error) {
-    console.error('An error occurred while filtering orders:', error);
-    filteredOrders = []; // hoặc bạn có thể gán giá trị mặc định khác nếu cần thiết
-  }
-
   const handleDelete = (orderId) => {
     dispatch(deleteOrder(orderId))
-      .then(() => {
-        dispatch(fetchOrders());
-      })
+      .then(() => dispatch(fetchOrders()))
       .catch((error) => {
         console.error("Error deleting order:", error);
       });
   };
+
+  if (loading) {
+    return (
+      <div className="order-management">
+        <h2>Orders</h2>
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Skeleton variant="rectangular" width={261} height={37} />
+                <Skeleton variant="rectangular" width={77.85} height={37} sx={{ ml: 2 }} />
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+            <Skeleton variant="rectangular" width="100%" height={64} />
+            </Grid>
+            <Grid item xs={12}>
+              <Skeleton variant="rectangular" width="100%" height={500} />
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
+
+  let filteredOrders = [];
+  if (orders) {
+    try {
+      filteredOrders = orders.filter((order) =>
+        order.email.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } catch (error) {
+      console.error('An error occurred while filtering orders:', error);
+    }
+  }
 
   return (
     <div className="order-management">
