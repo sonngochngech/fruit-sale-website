8cbf69cf863161ea33e1213e2f9a30a808e3742f
hasonsk <sonhh2003@gmail.com>
2024-05-19 07:07:17 +0700

    fix skeleton for categories

--- admin-app/src/pages/product/ProductManagement.js
+++ admin-app/src/pages/product/ProductManagement.js
@@ -11,20 +11,30 @@ import {
   updateProduct,
   deleteProduct,
 } from "../../features/products/productSlice";
+import {
+  fetchCategories,
+} from '../../features/categories/categorySlice';
+import { Skeleton, Box, Typography, Grid, Container } from '@mui/material';
 
 const ProductManagement = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products?.products);
+  const categories = useSelector((state) => state?.categories?.categories);
   const [showCreateProductForm, setShowCreateProductForm] = useState(false);
   const [showUpdateProductForm, setShowUpdateProductForm] = useState(false);
   const [updateProductData, setUpdateProductData] = useState(null);
   const [filteredProducts, setFilteredProducts] = useState(products);
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  console.log(products)
-  
   useEffect(() => {
-    dispatch(fetchProducts());
+    dispatch(fetchProducts())
+      .then(() => setLoading(false))
+      .catch((error) => {
+        console.error("Error fetching products:", error);
+        setLoading(false);
+      });
+    dispatch(fetchCategories());
   }, [dispatch]);
 
   useEffect(() => {
@@ -33,16 +43,16 @@ const ProductManagement = () => {
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    const filtered = (products.fruits).filter((product) => {
-      return product.title.toLowerCase().includes(event.target.value.toLowerCase())
-    }
-    );
+    const filtered = (products?.fruits || []).filter((product) => {
+      return product.title.toLowerCase().includes(event.target.value.toLowerCase());
+    });
     setFilteredProducts(filtered);
     if (filtered.length === 0) {
       // Show toast notification
       toast.warn("No products found with this search term.");
     }
   };
+
   const handleCreateProductClick = () => {
     setShowCreateProductForm(true);
   };
@@ -61,7 +71,7 @@ const ProductManagement = () => {
   };
 
   const handleUpdate = (productId) => {
-    const productToUpdate = products.fruits.find((product) => product.id === productId);
+    const productToUpdate = (products?.fruits || []).find((product) => product.id === productId);
     setUpdateProductData(productToUpdate);
     setShowUpdateProductForm(true);
   };
@@ -74,11 +84,36 @@ const ProductManagement = () => {
   const handleDelete = (productId) => {
     dispatch(deleteProduct(productId))
       .then(() => {
-        dispatch(fetchProducts())
-        // setFilteredProducts(updatedProducts);
+        dispatch(fetchProducts());
       })
       .catch((error) => console.error("Error deleting product:", error));
   };
+
+  if (loading) {
+    return (
+      <div className="product-management">
+        <h2>Products</h2>
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8.5}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Skeleton variant="rectangular" width={344} height={37} />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={3} sx={{ justifyContent: 'end', alignItems: 'center' }}>
+              <Skeleton variant="rectangular" width={175} height={53} />
+            </Grid>
+            <Grid item xs={12}>
+            <Skeleton variant="rectangular" width="100%" height={64} />
+            </Grid>
+            <Grid item xs={12}>
+              <Skeleton variant="rectangular" width="100%" height={400} />
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 
   return (
     <div className="product-management">
@@ -94,15 +129,6 @@ const ProductManagement = () => {
               value={searchTerm}
               onChange={handleSearch}
             />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary"
-                type="button"
-                onClick={handleSearch}
-              >
-                Search
-              </button>
-            </div>
           </div>
         </div>
         <div className="col-md-6 d-flex justify-content-end">
@@ -132,7 +158,7 @@ const ProductManagement = () => {
         </div>
       )}
       <ProductCard
-        products={filteredProducts.length > 0 ? filteredProducts : products.fruits}
+        products={filteredProducts.length > 0 ? filteredProducts : products?.fruits || []}
         onViewDetail={handleViewDetail}
         onEdit={handleUpdate}
         onDelete={handleDelete}
